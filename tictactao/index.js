0b870adca40fa6285379ceb12fa7d23a2fe17abb
Md Mahbub Alam <39045006+Mahbub96@users.noreply.github.com>
2021-04-26 21:39:43 +0600

    Update index.js

--- tictactao/index.js
+++ tictactao/index.js
@@ -1,11 +1,13 @@
+
 // Driver part 
+
+let turns = false;
 $(document).ready(function(){
-  AI_functionalities_Main();
+    
     $(".btn").click(function(){
       if($(this).text() == ""){
         
         $(this).text(human);
-        // console.log(parseInt(this.id))
         Board[parseInt(this.id)] = human;
         let iteration = 0;
         for(let i = 0; i < 3; i++){
@@ -26,4 +28,34 @@ $(document).ready(function(){
 });
 
 
+const  reset = () =>{
 
+$("#display").css("display","none");
+Board = ['','','','','','','','',''];
+board  = [['','',''],['','',''],['','','']];
+  for(let i = 0; i < 9; i++){
+    document.getElementById(i).innerText= ''
+  }
+  turns = !turns;
+  if(turns) AI_functionalities_Main();
+}
+
+const  Exit = () =>{
+  if(confirm("Really Want to Exit ?")) window.close();
+}
+
+const finalDecision = (d) =>{
+  console.log(d)
+  if(d == 1){
+    $("#display").css("display","block");
+    $("#result").text('You Lose !');
+  }
+  else if(d == -1){
+    $("#display").css("display","block");
+    $("#result").text('You Win !');
+  }
+  else if(d == 0){
+    $("#display").css("display","block");
+    $("#result").text('....');
+  }
+} 
